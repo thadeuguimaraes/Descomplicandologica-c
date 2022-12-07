feef53478de2e8ef45099077839b0c205a94e5a6
Thadeu84 <marciothadeu@gmail.com>
2022-12-07 13:50:51 -0300

    Arquivo C adicionados

--- Vetorpercidd.c
+++ Vetorpercidd.c
@@ -1,14 +1,81 @@
 #include <stdio.h>
-int main(void){
-    int idade [3] = {0, 0, 0};
-    float media = 0.0;
-    printf("\n Informe a idade da pessoa 1: ");
-    scanf("%d", &idade[0]);
-    printf("\n Informe a idade da pessoa 2: ");
-    scanf("%d", &idade[1]);
-    printf("\n Informe a idade da pessoa 3: ");
-    scanf("%d", &idade[2]);
-    media = (idade[0] + idade[1] + idade[2]) /3;
-    printf("\n Média de idade = %.2f", media);
-    return 0;
+#define VET_TAM 6
+
+float calcularMedia(float conjunto[], int tam)
+{
+    float soma = 0.0, resultado = 0.0;
+    for (int i = 0; i < tam; i++)
+    {
+        soma += conjunto[i];
+    }
+    resultado = soma / (float)tam;
+    return resultado;
 }
+
+float calcularMediana(float conjunto[], int tam)
+{
+    float resultado = 0.0;
+    if (tam % 2 != 0)
+    { // tam é impar
+        resultado = conjunto[tam / 2];
+    }
+    else
+    {
+        resultado = (conjunto[tam / 2] + conjunto[(tam / 2) - 1]) / 2;
+    }
+    return resultado;
+}
+
+int main(void)
+{
+    float vet[VET_TAM] = {1, 2, 3, 4, 5, 6};
+    float media = calcularMedia(vet, VET_TAM);
+    float mediana = calcularMediana(vet, VET_TAM);
+    printf("\nMédia = %.2f", media);
+    printf("\nMediana = %.2f", mediana);
+}
+
+// #include <stdio.h>
+
+// void inserir(int a[])
+// {
+//     int i = 0;
+//     for (i = 0; i < 3; i++)
+//     {
+//         printf("\nDigite o valor %d: ", i);
+//         scanf("%d", &a[i]);
+//     }
+// }
+
+// void imprimir(int b[])
+// {
+//     int i = 0;
+//     for (i = 0; i < 3; i++)
+//     {
+//         printf("\nNúmero[%d] = %d", i, 2 * b[i]);
+//     }
+// }
+// int main()
+// {
+//     int numeros[3];
+//     printf("\nPreenchendo o vetor... \n ");
+//     inserir(numeros);
+//     printf("\n\nDobro dos valores informados:");
+//     imprimir(numeros);
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main(void){
+//     int idade [3] = {0, 0, 0};
+//     float media = 0.0;
+//     printf("\n Informe a idade da pessoa 1: ");
+//     scanf("%d", &idade[0]);
+//     printf("\n Informe a idade da pessoa 2: ");
+//     scanf("%d", &idade[1]);
+//     printf("\n Informe a idade da pessoa 3: ");
+//     scanf("%d", &idade[2]);
+//     media = (idade[0] + idade[1] + idade[2]) /3;
+//     printf("\n Média de idade = %.2f", media);
+//     return 0;
+// }
--- funcsomar.c
+++ funcsomar.c
@@ -1,3 +1,91 @@
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+=======
+=======
+>>>>>>> Stashed changes
+#include <stdio.h>
+
+void testar(int *n1, int *n2)
+{
+    *n1 = -1; // o operador * permite acessar o valor da variável
+    *n2 = -2;
+    printf("\n\nValores dentro da função testar(): ");
+    printf("\nn1 = %d e n2 = %d", *n1, *n2);
+}
+
+int main()
+{
+    int n1 = 10;
+    int n2 = 20;
+    printf("\nValores antes de chamar a função testar(): ");
+    printf("\nn1 = %d e n2 = %d", n1, n2);
+
+    testar(&n1, &n2);
+
+    printf("\n\nValores depois de chamar a função testar(): ");
+    printf("\nn1 = %d e n2 = %d", n1, n2);
+
+    return 0;
+}
+
+// #include <stdio.h>
+
+// void testar(int n1, int n2)
+// {
+//     n1 = -1;
+//     n2 = -2;
+//     printf("\n\nValores dentro da função testar(): ");
+//     printf("\nn1 = % e n2 = %d", n1, n2);
+// }
+
+// int main()
+// {
+//     int n1 = 10;
+//     int n2 = 20;
+//     printf("\nValores antes de chamar a função testar(): ");
+//     printf("\nn1 = % e n2 = %d", n1, n2);
+
+//     testar(n1, n2);
+
+//     printf("\n\nValores depois de chamar a funçao testar(): ");
+//     printf("\nn1 = % e n2 = %d", n1, n2);
+
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int somar(int a, int b)
+// {
+//     return a + b;
+// }
+// int main()
+// {
+//     int result;
+//     result = somar(10, 15);
+//     printf("\nResultado da soma = %d", result);
+//     return 0;
+// }
+
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
+=======
+#include <stdio.h>
+int somar(int a, int b)
+{
+    return a + b;
+}
+int main()
+{
+    int result;
+    result = somar(10, 15);
+    printf("\nResultado da soma = %d", result);
+    return 0;
+}
+
+>>>>>>> Stashed changes
 // #include <stdio.h>
 // int somar()
 // {
